9797d864f0412beaa7eca88e7e0ccf6bfc9e2b39
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-24 13:38:35 -0300

    add

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -14,6 +14,7 @@ export class NegociacaoController {
     adiciona(): void {
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
+        
 
         this.limparFormulario();
       
--- app/models/nagociacoes.ts
+++ app/models/nagociacoes.ts
@@ -2,6 +2,18 @@ import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes{
     private negociacioes: Array<Negociacao> = [];
-    
+
+    adiciona(negociacao: Negociacao){
+        this.negociacioes.push(negociacao)
+    }
+
+    lista(): Array<Negociacao>{
+        return this.negociacioes;
+    }
+
 };
 
+const negociacoes = new Negociacoes();
+negociacoes.lista().forEach(n => {
+    
+})
