1f025d811b9f4c74fe63e27a3b3b680f2d6e014b
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-24 10:07:11 -0300

    add

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -11,16 +11,20 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona() {
-        const exp = /-/g;
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+      
+    }
+
+    criaNegociacao(): Negociacao{
+        const exp = /-/g; 
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
       
-        const negociacao = new Negociacao(date, quantidade, valor);
+        return new Negociacao(date, quantidade, valor);
 
-      console.log(negociacao);
-      
-    }
+    }   
 
 }
