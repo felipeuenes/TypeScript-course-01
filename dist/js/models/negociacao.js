26efe3dfab4733b59f0c3d9e1854ecd3690441b9
Felipe Uenes <uenesdesigner@gmail.com>
2023-08-08 15:18:58 -0300

    class

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,10 +1,8 @@
-import { Negociacoes } from "./nagociacoes.js";
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this.negociacoes = new Negociacoes();
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
     }
     get data() {
         return this._data;
